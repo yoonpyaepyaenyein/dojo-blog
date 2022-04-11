4dd03f6cea59d4b75c996454db130d65841041ff
yoonpyaepyaenyein <yoonpyae1310@gmail.com>
2022-04-11 18:07:05 +0630

    useEffectDependencies

--- src/Home.js
+++ src/Home.js
@@ -13,6 +13,8 @@ const Home = () => {
     },
   ]);
 
+  const [name, setName] = useState("mario");
+
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
@@ -20,11 +22,14 @@ const Home = () => {
 
   useEffect(() => {
     console.log("use effect ran");
-  });
+    console.log(name);
+  }, [name]);
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+      <button onClick={() => setName("luigi")}>change name</button>
+      <p>{name}</p>
     </div>
   );
 };
