75312b245d50c008c34f1690722670095c096f9a
yoonpyaepyaenyein <yoonpyae1310@gmail.com>
2022-04-12 21:04:37 +0630

    Route Parameters

--- src/Home.js
+++ src/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
     <div className="home">
       {error && <div>{error}</div>}
       {isPending && <div>Loading ....</div>}
-      {blogs && <BlogList blogs={blogs} title="All Blogs!" c />}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
   );
 };
