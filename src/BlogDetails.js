c8c175e7db2b50f965dc9e7689ad82afcc922c37
yoonpyaepyaenyein <yoonpyae1310@gmail.com>
2022-04-12 22:19:44 +0630

    Deleting Blogs

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,3 +1,4 @@
+import { useHistory } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
@@ -8,6 +9,15 @@ const BlogDetails = () => {
     error,
     isPending,
   } = useFetch("http://localhost:8000/blogs/" + id);
+  const history = useHistory();
+
+  const handleClick = () => {
+    fetch("http://localhost:8000/blogs/" + blog.id, {
+      method: "DELETE",
+    }).then(() => {
+      history.push("/");
+    });
+  };
 
   return (
     <div className="blog-details">
@@ -18,6 +28,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleClick}>Delete</button>
         </article>
       )}
     </div>
